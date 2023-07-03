82dee642a00ebbb40e73d2480f1913c075e91609
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-07-03 04:07:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 img="";
+var status="";
 function preload(){
     img=loadImage("dog_cat.jpg");
 }
 function setup(){
      canvas=createCanvas(640,420);
      canvas.center();
+     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+     document.getElementById("status").innerHTML="Status: objects are geting detected";
+}
+function modelLoaded(){
+    console.log("modelLoaded")
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+    console.log(results);
+    }
 }
 function draw(){
     image(img,0,0,640,420);
@@ -12,5 +28,11 @@ function draw(){
     fill("#2e91b8");
     stroke("#2e91b8");
     text("dog",30,50);
+    noFill();
+
+    rect(50,50,250,350);
+    fill("#a10c06");
+    stroke("#a10c06");
+    text("cat",280,50);
     noFill();
 }
